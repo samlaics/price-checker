b1dd85807dd37b33342d473a193a82046ba2847e
Samuel Lai <samlaics@gmail.com>
2016-06-15 16:29:57 -0700

    use prepared statements for adding new entries to db to prevent sql injection. also fix bug where urls with single quotes weren't being deleted

--- index.php
+++ index.php
@@ -14,24 +14,27 @@ if(! $conn ) {
 
 // show whether adding an entry was successful or not
 if( isset($_POST['add']) ) {
-	$item_link = $_POST['item_link'];
-	$sql = "INSERT INTO items (link) VALUES ('$item_link')";
-	$retval = mysqli_query($conn, $sql);
-	if(! $retval ) {
-  		die( 'Could not enter data: ' . mysqli_error($conn) );
+	$item_link = rawurlencode( $_POST['item_link']);
+	if ( $stmt = mysqli_prepare($conn, "INSERT INTO items (link) VALUES (?)") ) {
+		mysqli_stmt_bind_param($stmt, "s", $item_link);
+		mysqli_stmt_execute($stmt);
+		if( mysqli_stmt_affected_rows($stmt) != 1 ) {
+	  		die( 'Could not enter data: ' . mysqli_error($conn) );
+		}
+		mysqli_stmt_close($stmt);
+		echo "Entered data successfully <br />";
+		echo "<a href='index.php'>Back to Main Page</a>";
 	}
-	echo "Entered data successfully <br />";
-	echo "<a href='index.php'>Back to Main Page</a>";
 
 // show whether deleting an entry was successful or not
 } else if( isset($_POST['delete']) ) {
-	$id=$_POST['itemid'];
+	$id = $_POST['itemid'];
 	$sql = "DELETE FROM items WHERE link='$id'";
 	$retval = mysqli_query($conn, $sql);
 	if(! $retval ) {
   		die( 'Could not delete data: ' . mysqli_error($conn) );
 	}
-	echo "Deleted data successfully <br />";
+	echo "Deleted data successfully<br />";
 	echo "<a href='index.php'>Back to Main Page</a>";
 
 // main page: load item links from table and scrape prices from these sites
@@ -49,14 +52,15 @@ if( isset($_POST['add']) ) {
 	$sql = "SELECT link FROM items";
 	$result = mysqli_query($conn, $sql);
 	while ( $row = mysqli_fetch_array($result, MYSQLI_ASSOC) ) {
-		$html = file_get_contents($row['link']); 
+		$displayurl = rawurldecode($row['link']);
+		$html = file_get_contents($displayurl); 
 		$regex1 = '/<span id="priceblock_ourprice" class="a-size-medium a-color-price">(.+?)<\/span>/';
 	 	preg_match($regex1, $html, $price);
 	 	$regex2 = '/<span id="productTitle" class="a-size-large">[\n\s]*(.+?)[\n\s]*<\/span>/';
 	 	preg_match($regex2, $html, $product);
 	 	echo "<tr><td>".$product[1]."</td>";
 	 	echo "<td>".$price[1]."</td>";
-	    echo "<td><a href='".$row['link']."'>".$row['link']."</a></td>";
+	    echo "<td><a href='".$displayurl."'>".$displayurl."</a></td>";
 	    echo "<td><input type='hidden' name='itemid' value='".$row['link']."'><input type='submit' name='delete' value='Delete'></td></tr>";
 	}
     mysqli_free_result($result);
